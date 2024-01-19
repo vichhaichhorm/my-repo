c0a904a5debfd9d1bf909875b15b927f6d552b62
vichhaichhorm <155121433+vichhaichhorm@users.noreply.github.com>
2024-01-19 19:42:25 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+   public static void main(String[] args) {
+      // Input data by using scanner
+      System.out.println("====== Enter Your Information ======");
+      Scanner scanner = new Scanner(System.in);
+      System.out.print("Enter your ID: ");
+      int id = scanner.nextInt();
+      System.out.print("Enter your Name: ");
+      String name = scanner.next();
+      System.out.print("Enter Gender: ");
+      String gender = scanner.next();
+      System.out.print("Enter Age: ");
+      int age = scanner.nextInt();
+      System.out.print("Enter Your Class: ");
+      String classes = scanner.next();
+      System.out.print("Enter Your Course: ");
+      String course = scanner.next();
+      System.out.print("Enter Your Score: ");
+      float score = scanner.nextFloat();
+
+      // Show data
+      System.out.println("====== Show Information ======");
+      System.out.println("    ID: " +id);
+      System.out.println("  Name: " +name);
+      System.out.println("Gender: " +gender);
+      System.out.println("   Age: " +age);
+      System.out.println(" Class: " +classes);
+      System.out.println("Course: " +course);
+      System.out.println(" Score: " +score);
+
+   }
+}
